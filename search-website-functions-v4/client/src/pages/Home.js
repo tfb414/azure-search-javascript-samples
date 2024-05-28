8a48f2943a8abb1a7870a7246e601a3f0f67725e
Tim Brady <tfb414@gmail.com>
2024-05-28 10:17:46 -0600

    chore: test

--- search-website-functions-v4/client/src/pages/Home.js
+++ search-website-functions-v4/client/src/pages/Home.js
@@ -69,7 +69,7 @@ export default function Home() {
         </StyledImageContainer>
         <StyledTypographyContainer className="TypographyTextContainer">
           <StyledTypography className="Typography">
-            Powered by Azure AI Search
+            Powered by Azure AI Search tim
           </StyledTypography>
         </StyledTypographyContainer>
         <StyledHomeSearchBox className="StyledHomeSearchBox">
